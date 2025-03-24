0d452a01bcaec24c2029123aed368d920afd68ed
faelxrds77 <araujorafael114114@gmail.com>
2025-03-24 14:19:47 +0000

    novo teste

--- teste.c
+++ teste.c
@@ -1,63 +1,11 @@
 #include <stdio.h>
  
-int main(){
-    int idade,matricula;
-    float altura;
-    char nome[50];
+int main() {
+    char estado[30];
 
-    printf("Digite sua idade: \n");
-    scanf("%d", &idade);
 
-    printf("Digite sua altura: \n");
-    scanf("%f", &altura);
 
-    printf("Digite seu nome: \n");
-    scanf("%s", &nome);
-
-    printf("Digite sua matricula: \n");
-    scanf("%d", &matricula);
-
-    printf("Nome do aluno: %s\n - Matrícula: %d\n", nome, matricula);
-    printf("Idade: %d\n - Altura: %.2f\n", idade, altura);
-
-    int idade,matricula;
-    float altura;
-    char nome[50];
-
-    printf("Digite sua idade: \n");
-    scanf("%d", &idade);
-
-    printf("Digite sua altura: \n");
-    scanf("%f", &altura);
-
-    printf("Digite seu nome: \n");
-    scanf("%s", &nome);
-
-    printf("Digite sua matricula: \n");
-    scanf("%d", &matricula);
-
-    printf("Nome do aluno: %s\n - Matrícula: %d\n", nome, matricula);
-    printf("Idade: %d\n - Altura: %.2f\n", idade, altura);
-
+    printf("Digite seu estado: \n");
+    scanf("%s", &estado);
+    printf("Seu estado é: %s\n", estado);
     return 0;
-
-}
-
- 8
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
-
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizad
-
-
